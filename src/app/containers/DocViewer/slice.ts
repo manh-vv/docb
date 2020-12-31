c0eab4e509656258154e09f3a7f288e02db70f3d
manhvu <manhvu@orgos.net>
2020-12-31 17:47:02 +0700

    fix: :bug: fix bug on sidebar component

--- src/app/containers/DocViewer/slice.ts
+++ src/app/containers/DocViewer/slice.ts
@@ -1,49 +1,106 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { MenuItem } from 'types/MenuItem';
 
-import { ContainerState } from './types';
+import {
+  ContainerState,
+  FetchBigChapter,
+  FetchMdContent,
+  HideChildItems,
+  SelectedBook,
+  ShowChildItems,
+} from './types';
 
 // The initial state of the DocViewer container
 export const initialState: ContainerState = {
-  content: '',
   htmlContent: '',
   menuItems: [],
-  selectedBook: null,
-  lastActiveMenuItem: null,
 };
 
 const docViewerSlice = createSlice({
   name: 'docViewer',
   initialState,
   reducers: {
-    selectedBook(state, action: PayloadAction<any>) {
-      return {
-        ...state,
-        selectedBook: action.payload,
-      };
+    closeBook() {},
+    openBook(state, action: PayloadAction<SelectedBook>) {},
+    updateSelectedBook(state, action: PayloadAction<any>) {
+      state.selectedBook = action.payload;
     },
-    content(state, action: PayloadAction<any>) {
-      return {
-        ...state,
-        content: action.payload,
-      };
+    fetchBigChapter(state, action: PayloadAction<FetchBigChapter>) {},
+    fetchMdContent(state, action: PayloadAction<FetchMdContent>) {},
+    convertMdToHtml(state, action: PayloadAction<string>) {},
+    buildMenuItemsFromHtml() {},
+    restoreLastActiveMenuItems() {},
+    handleClickOnMenuItem(state, action: PayloadAction<MenuItem>) {},
+    showChildItems(state, action: PayloadAction<ShowChildItems>) {
+      const {
+        payload: { menuItems, menuItem },
+      } = action;
+
+      const index = state.menuItems.findIndex(item => item.id === menuItem.id);
+      if (index === -1) {
+        return;
+      }
+
+      state.menuItems = [
+        ...state.menuItems.slice(0, index + 1),
+        ...menuItems,
+        ...state.menuItems.slice(index + 1),
+      ];
     },
-    htmlContent(state, action: PayloadAction<any>) {
-      return {
-        ...state,
-        htmlContent: action.payload,
-      };
+    hideChildItems(state, action: PayloadAction<HideChildItems>) {
+      const {
+        payload: { menuItem },
+      } = action;
+
+      const set: Set<string> = new Set();
+      set.add(menuItem.id);
+
+      state.menuItems = state.menuItems.filter(item => {
+        const flag = item.parentId && set.has(item.parentId);
+        if (flag) {
+          set.add(item.id);
+        }
+        return !flag;
+      });
     },
-    menuItems(state, action: PayloadAction<any>) {
-      return {
-        ...state,
-        menuItems: action.payload || initialState.menuItems,
-      };
+    updateMenuItem(state, action: PayloadAction<MenuItem>) {
+      const { payload: menuItem } = action;
+      const index = state.menuItems.findIndex(item => item.id === menuItem.id);
+      if (index === -1) return;
+
+      if (menuItem.active) {
+        const lastActiveMenuItem = state.lastActiveMenuItem;
+
+        if (lastActiveMenuItem) {
+          const lastActiveIndex = state.menuItems.findIndex(
+            item => item.id === lastActiveMenuItem.id,
+          );
+
+          if (lastActiveIndex !== -1 && lastActiveIndex !== index) {
+            if (!lastActiveMenuItem.childIds || !lastActiveMenuItem.childIds.length) {
+              state.menuItems = [
+                ...state.menuItems.slice(0, lastActiveIndex),
+                { ...lastActiveMenuItem, active: false },
+                ...state.menuItems.slice(lastActiveIndex + 1),
+              ];
+            }
+          }
+        }
+
+        state.lastActiveMenuItem = menuItem;
+      }
+
+      state.menuItems = [
+        ...state.menuItems.slice(0, index),
+        menuItem,
+        ...state.menuItems.slice(index + 1),
+      ];
     },
-    lastActiveMenuItem(state, action: PayloadAction<any>) {
-      return {
-        ...state,
-        lastActiveMenuItem: action.payload,
-      };
+    updateHtmlContent(state, action: PayloadAction<string>) {
+      state.htmlContent = action.payload;
+    },
+    updateMenuItems(state, action: PayloadAction<MenuItem[]>) {
+      state.menuItems = action.payload;
     },
   },
 });
--- src/app/containers/DocViewer/types.ts
+++ src/app/containers/DocViewer/types.ts
@@ -1,18 +1,36 @@
-/* --- STATE --- */
+import { MenuItem } from 'types/MenuItem';
+
+export interface ShowChildItems {
+  menuItem: MenuItem;
+  menuItems: MenuItem[];
+}
+export interface HideChildItems {
+  menuItem: MenuItem;
+}
+
+export interface FetchBigChapter {
+  provider: string;
+  username: string;
+  repository: string;
+  base64FilePath?: string;
+}
+export interface FetchMdContent {
+  provider: string;
+  username: string;
+  repository: string;
+  base64FilePath: string;
+}
+export interface SelectedBook {
+  provider: string;
+  username: string;
+  repository?: string;
+  base64FilePath?: string;
+}
 export interface DocViewerState {
-  content: string;
   htmlContent: string;
-  menuItems: any[];
-  selectedBook: {
-    provider: string;
-    username: string;
-    repository?: string;
-    base64FilePath?: string;
-  } | null;
-  lastActiveMenuItem: {
-    menuItem: string;
-    index: number;
-  } | null;
+  menuItems: MenuItem[];
+  selectedBook?: SelectedBook;
+  lastActiveMenuItem?: MenuItem;
 }
 
 export type ContainerState = DocViewerState;
